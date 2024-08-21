032515b2d5de451c9a3f25bcae3bdaf3e02ef67e
NomFascinant <thinksright8@gmail.com>
2024-08-21 14:52:22 +0200

    Add files via upload

---
+++ Fonction/Utils.py
@@ -0,0 +1,22 @@
+from colorama import *
+import tempfile
+import os
+import subprocess
+import winshell
+import webbrowser
+import psutil
+import platform
+from datetime import datetime
+
+reset = Style.RESET_ALL
+bright = Style.BRIGHT
+white = Fore.WHITE
+green = Fore.GREEN
+red = Fore.RED 
+magenta = Fore.MAGENTA
+yellow = Fore.YELLOW
+cyan = Fore.CYAN
+blue = Fore.BLUE
+gray = Fore.LIGHTBLACK_EX
+
+
